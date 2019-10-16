1410b082bc0ebd301da843b556fceb769f7047f8
Guilherme henrique <progr.guilherme@gmail.com>
2019-10-16 13:10:54 -0300

    WORKING EXAMPLE

--- script.js
+++ script.js
@@ -1,75 +1,27 @@
-// message mock
-var dates = [
-    {
-       "id":88,
-       "listConversa":[
-          {
-             "id":232,
-             "chatCodigo":"9f98675a-bf9e-43b2-9a6d-9412bf63d406",
-             "nome":"Afonso Henrique Ferreira Cunha",
-             "email":"af@e.com",
-             "mensagem":"Alimentação",
-             "tsCriacao":"2019-10-14T17:10:27Z",
-             "origem":"CLIENTE",
-             "idColaborador":null,
-             "nomeColaborador":null
-          },
-          {
-             "id":233,
-             "chatCodigo":"9f98675a-bf9e-43b2-9a6d-9412bf63d406",
-             "nome":"Guilherme",
-             "email":"guilherme.machado@univision.net.br",
-             "mensagem":"Olá como posso ajuda?",
-             "tsCriacao":"2019-10-15T17:12:06Z",
-             "origem":"COLABORADOR",
-             "idColaborador":38,
-             "nomeColaborador":"Guilherme"
-          },
-       ]
-    }
-  ];
-  
-  // Generate a dates array given a start date and how many to create:
-  function genDates(startDate, count) {
-    var d = new Date(startDate)
-        dates = [d];
-    for (var i=0; i<count; i++) {
-      d = new Date(+d);
-      d.setHours(d.getHours() + 10);
-      dates.push(d);
-    }
-    return dates;
-  }
+var dates = {"id":88,"codigo":"9f98675a-bf9e-43b2-9a6d-9412bf63d406","sistema":"QUER","codigoSingular":"0032","emailCliente":"af@e.com","nomeCliente":"Afonso Henrique Ferreira Cunha","idPessoa":"43ZIf0s9l2siHvlI8lN1","nomeColaborador":"Guilherme","situacao":"EM_ATENDIMENTO","assunto":"Alimentação","tsConclusao":null,"tsCriacao":"2019-10-15T17:10:27Z","listConversa":[{"id":232,"chatCodigo":"9f98675a-bf9e-43b2-9a6d-9412bf63d406","nome":"Afonso Henrique Ferreira Cunha","email":"af@e.com","mensagem":"Alimentação","tsCriacao":"2019-10-15T17:10:27Z","origem":"CLIENTE","idColaborador":null,"nomeColaborador":null},{"id":233,"chatCodigo":"9f98675a-bf9e-43b2-9a6d-9412bf63d406","nome":"Guilherme","email":"guilherme.machado@univision.net.br","mensagem":"Olá como posso ajuda?","tsCriacao":"2019-10-15T17:12:06Z","origem":"COLABORADOR","idColaborador":38,"nomeColaborador":"Guilherme"},{"id":234,"chatCodigo":"9f98675a-bf9e-43b2-9a6d-9412bf63d406","nome":"Afonso Henrique Ferreira Cunha","email":"af@e.com","mensagem":"Quais cuidados devo ter com a alimentação? ","tsCriacao":"2019-10-15T17:12:22Z","origem":"CLIENTE","idColaborador":null,"nomeColaborador":null},{"id":235,"chatCodigo":"9f98675a-bf9e-43b2-9a6d-9412bf63d406","nome":"Guilherme","email":"guilherme.machado@univision.net.br","mensagem":"The Ionic CLI can build, copy, and deploy Ionic apps to Android simulators and devices with a single command. It can also spin up a development server, like the one used in ionic serve, to provide live-reload functionality.","tsCriacao":"2019-10-15T17:22:17Z","origem":"COLABORADOR","idColaborador":38,"nomeColaborador":"Guilherme"},{"id":236,"chatCodigo":"9f98675a-bf9e-43b2-9a6d-9412bf63d406","nome":"Afonso Henrique Ferreira Cunha","email":"af@e.com","mensagem":"Teste com um cuidador ","tsCriacao":"2019-10-15T17:23:19Z","origem":"CLIENTE","idColaborador":null,"nomeColaborador":null},{"id":237,"chatCodigo":"9f98675a-bf9e-43b2-9a6d-9412bf63d406","nome":"Guilherme","email":"guilherme.machado@univision.net.br","mensagem":"DevApp offers a realtime view of changes as they're being made, with a rich library of pre-installed native plugins to test native features of the app.\n","tsCriacao":"2019-10-15T17:23:46Z","origem":"COLABORADOR","idColaborador":38,"nomeColaborador":"Guilherme"},{"id":238,"chatCodigo":"9f98675a-bf9e-43b2-9a6d-9412bf63d406","nome":"Guilherme","email":"guilherme.machado@univision.net.br","mensagem":"When testing an application, it is best to keep in mind that testing can show if defects are present in a system. However, it is impossible to prove that any non-trivial system is","tsCriacao":"2019-10-15T17:24:27Z","origem":"COLABORADOR","idColaborador":38,"nomeColaborador":"Guilherme"},{"id":240,"chatCodigo":"9f98675a-bf9e-43b2-9a6d-9412bf63d406","nome":"Afonso Henrique Ferreira Cunha","email":"af@e.com","mensagem":"esse componente de card ja ta pronto, só colocar os inputs na tela, criar o redux e o saga e chamar a api","tsCriacao":"2019-10-15T17:30:16Z","origem":"CLIENTE","idColaborador":null,"nomeColaborador":null},{"id":241,"chatCodigo":"9f98675a-bf9e-43b2-9a6d-9412bf63d406","nome":"Afonso Henrique Ferreira Cunha","email":"af@e.com","mensagem":"dia 16 ","tsCriacao":"2019-10-16T14:47:29Z","origem":"CLIENTE","idColaborador":null,"nomeColaborador":null}]}
 
-
-  var generatedData = genDates(dates[0].listConversa[0].tsCriacao, dates[0].listConversa.length)
-  // Generate date key 'MMM dd'
-  // Replaces use of moment.js
-  function getDateKey(date) {
-    var d = date.getDate();
-    var m = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-    return m[date.getMonth()] + ' ' + ((d<10?'0':'') + d);
-  }
+// Generate date key 'MMM dd'
+function getDateKey(date) {
+  var d = new Date(date.tsCriacao).getDate();
+  var m = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+  return m[new Date(date.tsCriacao).getMonth()] + ' ' + ((d<10?'0':'') + d);
+}
   
-  // Generate an array in format [{day:'MMM dd', times:[d0, d1, ...]}, ...]
-  // Replaces use of underscore.js
-  var obj = dates.reduce(function(acc, d) {
-              var p = getDateKey(d)
-              if (!acc[0].hasOwnProperty(p)) acc[0][p] = [];
-              acc[0][p].push(d);
-              return acc;
-            },[{}])
-            .reduce(function(acc, v){
-              Object.keys(v).forEach(function(k){
-                acc.push({
-                  day:k,
-                  times:v[k]
-                })
-              });
-              return acc;
-            },[]);
+// Generate an array in format [{day:'MMM dd', times:[d0, d1, ...]}, ...]
+var obj = dates.listConversa.reduce(function(acc, d) {
+  var p = getDateKey(d)
+  if (!acc[0].hasOwnProperty(p)) acc[0][p] = [];
+  acc[0][p].push(d);
+  return acc;
+},[{}])
+  .reduce(function(acc, v){
+    Object.keys(v).forEach(function(k){
+      acc.push({
+        day:k,
+        times:v[k]
+      })
+    });
+  return acc;
+  },[]);
   
-console.warn('obj =>');
-console.table(obj);
-console.warn('generatedData =>');
-console.table(generatedData);
+console.log(obj);
